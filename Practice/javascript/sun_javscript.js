340809f8a42d6cd1b578d7f2a49a7efd00734821
vikasreddycode <vikasmudimela@gmail.com>
2023-04-14 20:46:48 +0530

    object accessing

--- Practice/javascript/sun_javscript.js
+++ Practice/javascript/sun_javscript.js
@@ -1,3 +1,11 @@
-// 
-let userlogin = false;
-userlogin?console.log("you have logged in"):console.log("you have not logged in register");
+// // 
+// let userlogin = false;
+// userlogin?console.log("you have logged in"):console.log("you have not logged in register");
+// let names = ["vk","mk","rk","lk","jk","7y","vikram","mahesh"];
+// console.log(names[6]);
+// accesing names throgh array index
+let bankaccount = {
+    fname:"vikas",lname:"reddy",nname:"vicky"
+}
+console.log(bankaccount.fname);
+console.log(bankaccount.nname);
