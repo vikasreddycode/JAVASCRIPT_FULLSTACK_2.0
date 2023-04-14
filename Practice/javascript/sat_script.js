81100526cd33afd2604fa0e5394f2528966ce7c3
vikasreddycode <vikasmudimela@gmail.com>
2023-04-14 17:39:19 +0530

    new error interview

--- Practice/javascript/sat_script.js
+++ Practice/javascript/sat_script.js
@@ -19,8 +19,10 @@
 // // console.log(x%y)
 // // console.log(x>y);
 // // console.log(x<y);
-let abc = 30;
-let xyz = '30';
-// the difference in abc and xyz is both are same values but different data types == only checks the values but === checks values and datatypes
-console.log(abc==xyz); 
-console.log(abc===xyz);
+// let abc = 30;
+// let xyz = '30';
+// // the difference in abc and xyz is both are same values but different data types == only checks the values but === checks values and datatypes
+// console.log(abc==xyz); 
+// console.log(abc===xyz);
+// const mk;
+// mk = 32
