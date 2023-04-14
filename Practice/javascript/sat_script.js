bffe861ef1796193f659508ed05d077be8735d43
vikasreddycode <vikasmudimela@gmail.com>
2023-04-14 17:29:01 +0530

    the major difference between == and ===

--- Practice/javascript/sat_script.js
+++ Practice/javascript/sat_script.js
@@ -1,11 +1,26 @@
-// the comments in javascript
-let fname = "vikas";
-let lname = "reddy";
-const mbno = 6304297456876;
-// console.log(fname);
-// console.log(lname);
-// console.log(mbno);
-// console.log("my first name is",fname);
-// console.log("my last name is",lname);
-// console.log("my mobile number is",mbno);
-console.log(`my name is ${fname},${lname},${mbno}`);
+// // the comments in javascript
+// let fname = "vikas";
+// let lname = "reddy";
+// const mbno = 6304297456876;
+// // console.log(fname);
+// // console.log(lname);
+// // console.log(mbno);
+// // console.log("my first name is",fname);
+// // console.log("my last name is",lname);
+// // console.log("my mobile number is",mbno);
+// // console.log(`my name is ${fname},${lname},${mbno}`);
+// let x = 30;
+// let y = 50;
+// // let z = x+y;
+// // console.log(z);
+// // console.log(x+y);
+// // console.log(x-y);
+// // console.log(x/y);
+// // console.log(x%y)
+// // console.log(x>y);
+// // console.log(x<y);
+let abc = 30;
+let xyz = '30';
+// the difference in abc and xyz is both are same values but different data types == only checks the values but === checks values and datatypes
+console.log(abc==xyz); 
+console.log(abc===xyz);
