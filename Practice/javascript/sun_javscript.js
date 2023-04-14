53ca78b06890a0aa0fd5d6adb42733824a73e21d
vikasreddycode <vikasmudimela@gmail.com>
2023-04-14 20:22:59 +0530

    terneary operator

--- Practice/javascript/sun_javscript.js
+++ Practice/javascript/sun_javscript.js
@@ -1,12 +1,3 @@
-let role = "admin";
-switch(role)
-{
-    case "admin":
-        console.log("you are an admin");
-        break;
-    case "mentor":
-        console.log("you are a mentor");
-        break;
-    default:
-        console.log("new user");
-}
+// 
+let userlogin = false;
+userlogin?console.log("you have logged in"):console.log("you have not logged in register");
