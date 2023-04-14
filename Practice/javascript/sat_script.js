b4f35a482541263ac4c9635e38eace8e5c6308d0
vikasreddycode <vikasmudimela@gmail.com>
2023-04-14 16:46:14 +0530

    print statemnets in javascript

--- Practice/javascript/sat_script.js
+++ Practice/javascript/sat_script.js
@@ -1,3 +1,10 @@
 // the comments in javascript
-let fname = "vikas"
-let lname = "reddy"
+let fname = "vikas";
+let lname = "reddy";
+const mbno = 6304297456876;
+console.log(fname);
+console.log(lname);
+console.log(mbno);
+console.log("my first name is",fname);
+console.log("my last name is",lname);
+console.log("my mobile number is",mbno);
