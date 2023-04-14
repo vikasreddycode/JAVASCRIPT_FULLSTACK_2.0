9cae2a4defd58ad5421ea5c33303aa65d8ebcb1d
vikasreddycode <vikasmudimela@gmail.com>
2023-04-14 16:54:02 +0530

    template literal

--- Practice/javascript/sat_script.js
+++ Practice/javascript/sat_script.js
@@ -2,9 +2,10 @@
 let fname = "vikas";
 let lname = "reddy";
 const mbno = 6304297456876;
-console.log(fname);
-console.log(lname);
-console.log(mbno);
-console.log("my first name is",fname);
-console.log("my last name is",lname);
-console.log("my mobile number is",mbno);
+// console.log(fname);
+// console.log(lname);
+// console.log(mbno);
+// console.log("my first name is",fname);
+// console.log("my last name is",lname);
+// console.log("my mobile number is",mbno);
+console.log(`my name is ${fname},${lname},${mbno}`);
