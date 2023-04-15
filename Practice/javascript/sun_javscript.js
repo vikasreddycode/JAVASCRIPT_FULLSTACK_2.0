682caad8ea83e8fd4945186a486aa2487c0c99ec
vikasreddycode <vikasmudimela@gmail.com>
2023-04-15 08:25:47 +0530

    Math module added

--- Practice/javascript/sun_javscript.js
+++ Practice/javascript/sun_javscript.js
@@ -1,11 +1,28 @@
-// // 
-// let userlogin = false;
-// userlogin?console.log("you have logged in"):console.log("you have not logged in register");
-// let names = ["vk","mk","rk","lk","jk","7y","vikram","mahesh"];
-// console.log(names[6]);
-// accesing names throgh array index
-// let bankaccount = {
-//     fname:"vikas",lname:"reddy",nname:"vicky"
-// }
-// console.log(bankaccount.fname);
-// console.log(bankaccount.nname);
+// // // // 
+// // // let userlogin = false;
+// // // userlogin?console.log("you have logged in"):console.log("you have not logged in register");
+// // // let names = ["vk","mk","rk","lk","jk","7y","vikram","mahesh"];
+// // // console.log(names[6]);
+// // // accesing names throgh array index
+// // // let bankaccount = {
+// // //     fname:"vikas",lname:"reddy",nname:"vicky"
+// // // }
+// // // console.log(bankaccount.fname);
+// // // console.log(bankaccount.nname);
+// // const v = new Date();
+// // console.log(v);
+// // console.log(v.toString());
+// // console.log(v.getTime());
+// // console.log(v.getHours());
+// // console.log(v.getMinutes());
+// // console.log(v.getSeconds());
+// const pi = Math.PI;
+// console.log(pi);
+// console.log(Math.round(Math.PI));
+// console.log(Math.round(3.5));
+// console.log(Math.round(3.7));
+// // console.log(Math.r)
+console.log(Math.ceil(9.5));
+console.log(Math.floor(9.5));
+console.log(Math.min(1,2,3,4,5,6));
+console.log(Math.max(7,6,5,4,3,2,1));
