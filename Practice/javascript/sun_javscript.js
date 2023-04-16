64c4df24962372b95c3f35307c29baefb8515e92
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 09:04:10 +0530

    new arrays

--- Practice/javascript/sun_javscript.js
+++ Practice/javascript/sun_javscript.js
@@ -1,46 +1,46 @@
-// // // // 
-// // // let userlogin = false;
-// // // userlogin?console.log("you have logged in"):console.log("you have not logged in register");
-// // // let names = ["vk","mk","rk","lk","jk","7y","vikram","mahesh"];
-// // // console.log(names[6]);
-// // // accesing names throgh array index
-// // // let bankaccount = {
-// // //     fname:"vikas",lname:"reddy",nname:"vicky"
-// // // }
-// // // console.log(bankaccount.fname);
-// // // console.log(bankaccount.nname);
-// // const v = new Date();
-// // console.log(v);
-// // console.log(v.toString());
-// // console.log(v.getTime());
-// // console.log(v.getHours());
-// // console.log(v.getMinutes());
-// // console.log(v.getSeconds());
-// const pi = Math.PI;
-// console.log(pi);
-// console.log(Math.round(Math.PI));
-// console.log(Math.round(3.5));
-// console.log(Math.round(3.7));
-// // console.log(Math.r)
-// console.log(Math.ceil(9.5));
-// console.log(Math.floor(9.5));
-// console.log(Math.min(1,2,3,4,5,6));
-// console.log(Math.max(7,6,5,4,3,2,1));
-// console.log(Math.random()*10);
-// console.log(Math.pow(3,2));
-let i = 0;
-let j = 0;
-do
-{
-console.log(`the new thing is ${i} `);
-i++;
-}while(i<=5);
-while(j<=5)
-{
-    console.log(`the new value is ${j} `);
-    j++;
-}
-for(let i=0 ;i<=5;i++)
-{
-    console.log(`the value ${i}*${i}=${i*i}`);
-}
+// // // // // 
+// // // // let userlogin = false;
+// // // // userlogin?console.log("you have logged in"):console.log("you have not logged in register");
+// // // // let names = ["vk","mk","rk","lk","jk","7y","vikram","mahesh"];
+// // // // console.log(names[6]);
+// // // // accesing names throgh array index
+// // // // let bankaccount = {
+// // // //     fname:"vikas",lname:"reddy",nname:"vicky"
+// // // // }
+// // // // console.log(bankaccount.fname);
+// // // // console.log(bankaccount.nname);
+// // // const v = new Date();
+// // // console.log(v);
+// // // console.log(v.toString());
+// // // console.log(v.getTime());
+// // // console.log(v.getHours());
+// // // console.log(v.getMinutes());
+// // // console.log(v.getSeconds());
+// // const pi = Math.PI;
+// // console.log(pi);
+// // console.log(Math.round(Math.PI));
+// // console.log(Math.round(3.5));
+// // console.log(Math.round(3.7));
+// // // console.log(Math.r)
+// // console.log(Math.ceil(9.5));
+// // console.log(Math.floor(9.5));
+// // console.log(Math.min(1,2,3,4,5,6));
+// // console.log(Math.max(7,6,5,4,3,2,1));
+// // console.log(Math.random()*10);
+// // console.log(Math.pow(3,2));
+// let i = 0;
+// let j = 0;
+// do
+// {
+// console.log(`the new thing is ${i} `);
+// i++;
+// }while(i<=5);
+// while(j<=5)
+// {
+//     console.log(`the new value is ${j} `);
+//     j++;
+// }
+// for(let i=0 ;i<=5;i++)
+// {
+//     console.log(`the value ${i}*${i}=${i*i}`);
+// }
