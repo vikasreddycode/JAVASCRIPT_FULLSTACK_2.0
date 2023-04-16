5e11d5aadeb54d4ee680a822f7d43cadd770b8b7
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 17:46:25 +0530

    unshift method

--- Practice/javascript/arrays.js
+++ Practice/javascript/arrays.js
@@ -1,9 +1,13 @@
-// 
-let arr3 = [1,2,3,4,5,6,7];
-console.log(arr3.map(Math.sqrt));
-// for(let i=0;i<7;i++)
-// {
-//     console.log(arr3.pop());
-// }
-// console.log(arr3.pop())
-console.log(arr3.reverse());
+// // 
+// let arr3 = [1,2,3,4,5,6,7];
+// console.log(arr3.map(Math.sqrt));
+// // for(let i=0;i<7;i++)
+// // {
+// //     console.log(arr3.pop());
+// // }
+// // console.log(arr3.pop())
+// console.log(arr3.reverse());
+let names = ["vikcy","uncle","anan","hdd","shsjks","sghssh","gsysgs"];
+console.log(names.sort());
+console.log(names.unshift("vicky 1","vicky 2"));
+console.log(names);
