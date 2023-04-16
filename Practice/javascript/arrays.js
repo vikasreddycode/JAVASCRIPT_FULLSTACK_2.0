167c2e54096f552657c1a5abba0e1b8f0af7f2d4
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 10:17:23 +0530

    newslicepushandslicemethod

--- Practice/javascript/arrays.js
+++ Practice/javascript/arrays.js
@@ -1,9 +1,16 @@
-let names  = ['vinay','vikas','hello','good'];
-console.log(names);
-// length of an array
-console.log(names.length);
-// array length -1
-console.log(names.length-1);
-// array lenght -1 
-names[3]="vikcy";
-console.log(names);
+// let names  = ['vinay','vikas','hello','good'];
+// console.log(names);
+// // length of an array
+// console.log(names.length);
+// // array length -1
+// console.log(names.length-1);
+// // array lenght -1 
+// names[3]="vikcy";
+// // console.log(names);
+
+let arr =  new Array("vikcu","mk",'kk','hello');
+console.log(arr);
+arr.push("hsasaksj");
+arr.push("helllomawa");
+console.log(arr)
+console.log(arr.slice(2,5))
