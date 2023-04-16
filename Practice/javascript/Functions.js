924e62e9d0695a46c98c014c33c4ea2f06635c1b
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 21:13:32 +0530

    function return type

--- Practice/javascript/Functions.js
+++ Practice/javascript/Functions.js
@@ -1,15 +1,26 @@
-// function sum(n1,n2)
+// // function sum(n1,n2)
+// // {
+// //     console.log(`the sum of ${n1} and ${n2} is ${n1+n2}`);
+// // }
+// // for(let i=1;i<=7;i++)
+// // {
+// //     sum(i,i+1);
+// // }
+// function sum2(n1,n2)
 // {
-//     console.log(`the sum of ${n1} and ${n2} is ${n1+n2}`);
+//     return (n1+n2);
 // }
-// for(let i=1;i<=7;i++)
-// {
-//     sum(i,i+1);
-// }
-function sum2(n1,n2)
+// let j = sum2(5,6);
+// console.log(j);
+// console.log(sum2(33,44));
+function sum(arr)
 {
-    return (n1+n2);
+    let sum=0;
+    for(const i of arr)
+    {
+        sum=sum+i;
+    }
+    return sum;
 }
-let j = sum2(5,6);
-console.log(j);
-console.log(sum2(33,44));
+let h =sum([22,33,44,55,66]);
+console.log(h);
