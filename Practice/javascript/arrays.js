e72ec8a494955f726bc078d15dff871c3440d444
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 09:28:01 +0530

    new array methods

---
+++ Practice/javascript/arrays.js
@@ -0,0 +1,9 @@
+let names  = ['vinay','vikas','hello','good'];
+console.log(names);
+// length of an array
+console.log(names.length);
+// array length -1
+console.log(names.length-1);
+// array lenght -1 
+names[3]="vikcy";
+console.log(names);
