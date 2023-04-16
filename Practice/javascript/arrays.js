57f5124016ed046354619390b0245c622f17f063
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 20:46:48 +0530

    newfunction created

--- Practice/javascript/arrays.js
+++ Practice/javascript/arrays.js
@@ -1,15 +1,39 @@
-// // 
-// let arr3 = [1,2,3,4,5,6,7];
-// console.log(arr3.map(Math.sqrt));
-// // for(let i=0;i<7;i++)
-// // {
-// //     console.log(arr3.pop());
-// // }
-// // console.log(arr3.pop())
-// console.log(arr3.reverse());
-let names = ["vikcy","uncle","anan","hdd","shsjks","sghssh","gsysgs"];
-console.log(names.sort());
-console.log(names.unshift("vicky 1","vicky 2"));
-console.log(names);
-let name = "vicky is a namial";
-console.log(name.split(" "))
+// // // // 
+// // // let arr3 = [1,2,3,4,5,6,7];
+// // // console.log(arr3.map(Math.sqrt));
+// // // // for(let i=0;i<7;i++)
+// // // // {
+// // // //     console.log(arr3.pop());
+// // // // }
+// // // // console.log(arr3.pop())
+// // // console.log(arr3.reverse());
+// // let names = ["vikcy","uncle","anan","hdd","shsjks","sghssh","gsysgs"];
+// // console.log(names.sort());
+// // console.log(names.unshift("vicky 1","vicky 2"));
+// // console.log(names);
+// // let name = "vicky is a namial";
+// // console.log(name.split(" "))
+// let mk =  ['hello','new','vsihsd'];
+// ml = [];
+// for (const j of mk)
+// {
+//     ml.push(j.toUpperCase())
+// }
+// console.log(ml);
+let januma = "vicky";
+for(const i of januma)
+{
+    console.log(i);
+}
+for(let i =0;i<7;i++)
+{
+    if(i<=2)
+    {
+        continue;
+    }
+    if(i==5)
+    {
+        break;
+    }
+    console.log(i);
+}
