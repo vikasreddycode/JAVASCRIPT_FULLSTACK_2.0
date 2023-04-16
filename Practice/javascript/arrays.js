2e2da3666d9e1a00d6ad912abcaa039c87338527
vikasreddycode <vikasmudimela@gmail.com>
2023-04-16 17:31:01 +0530

    reverse method

--- Practice/javascript/arrays.js
+++ Practice/javascript/arrays.js
@@ -1,42 +1,9 @@
-// // let names  = ['vinay','vikas','hello','good'];
-// // console.log(names);
-// // // length of an array
-// // console.log(names.length);
-// // // array length -1
-// // console.log(names.length-1);
-// // // array lenght -1 
-// // names[3]="vikcy";
-// // // console.log(names);
-
-// // let arr =  new Array("vikcu","mk",'kk','hello');
-// // console.log(arr);
-// // arr.push("hsasaksj");
-// // arr.push("helllomawa");
-// // console.log(arr);
-// // console.log(arr.slice(2,5));
-// // let fruit  = ['Apple','Bad A','CAD A','VAD A','DAS A']
-// // fruit.splice(2,1,'MK','RK');
-// // console.log(fruit);
-// // fruit.splice(2,2,"mk","rk");
-// // console.log(fruit);
-// let arr31 = [1,2,3,4,5,6];
-// let arr32 = [7,8,9];
-// let arr43 = [10,11,12,13];
-// console.log(arr31.concat(arr32));
-// console.log(arr31.concat(arr32,arr43));
-// let arr4 = [1,2,3,4,5,6,7,8];
-// arr4.fill('Vikas',2,4);
-// console.log(arr4);
-// arr4.fill("new","old",2,4);
-// console.log(arr4);
+// 
 let arr3 = [1,2,3,4,5,6,7];
-// console.log(arr3.includes(5,4));
-let arr4 = [1,23,4,423,44,223,"vicky",444,44,"vicky"];
-// console.log(arr4.indexOf("vicky"));
-let n1 = 3;
-// console.log(Array.isArray(arr4));
-// console.log(Array.isArray(n1));
-// console.log(arr4.join("||"))
-let arr5 = [1];
-console.log(arr5.join("new"));
-console.log(arr4.lastIndexOf("vicky"));
+console.log(arr3.map(Math.sqrt));
+// for(let i=0;i<7;i++)
+// {
+//     console.log(arr3.pop());
+// }
+// console.log(arr3.pop())
+console.log(arr3.reverse());
