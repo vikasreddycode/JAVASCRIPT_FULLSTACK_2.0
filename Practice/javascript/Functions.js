6d113cb539f1f95ea3a1e5c74e18c72ca4045e4a
vikasreddycode <vikasmudimela@gmail.com>
2023-04-18 10:30:32 +0530

    lambda function

--- Practice/javascript/Functions.js
+++ Practice/javascript/Functions.js
@@ -1,11 +1,15 @@
-// 
-function nowvalue()
-{
-    let  sum  = 0;
-    for(const i of arguments)
-{
-    sum = sum + i;
+// // 
+// function nowvalue()
+// {
+//     let  sum  = 0;
+//     for(const i of arguments)
+// {
+//     sum = sum + i;
+// }
+// return sum;
+// }
+// console.log(nowvalue(6,5,4,3,2,4));
+let name = (v1,v2) => {
+    console.log("this is a lambda function with parameters",v1,v2);
 }
-return sum;
-}
-console.log(nowvalue(6,5,4,3,2,4));
+name(2,3);
