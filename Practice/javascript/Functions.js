6615df5129c75b753e799dc714abd07e5e0f035e
vikasreddycode <vikasmudimela@gmail.com>
2023-04-18 10:24:18 +0530

    new value needed

--- Practice/javascript/Functions.js
+++ Practice/javascript/Functions.js
@@ -1,26 +1,11 @@
-// // function sum(n1,n2)
-// // {
-// //     console.log(`the sum of ${n1} and ${n2} is ${n1+n2}`);
-// // }
-// // for(let i=1;i<=7;i++)
-// // {
-// //     sum(i,i+1);
-// // }
-// function sum2(n1,n2)
-// {
-//     return (n1+n2);
-// }
-// let j = sum2(5,6);
-// console.log(j);
-// console.log(sum2(33,44));
-function sum(arr)
+// 
+function nowvalue()
 {
-    let sum=0;
-    for(const i of arr)
-    {
-        sum=sum+i;
-    }
-    return sum;
+    let  sum  = 0;
+    for(const i of arguments)
+{
+    sum = sum + i;
 }
-let h =sum([22,33,44,55,66]);
-console.log(h);
+return sum;
+}
+console.log(nowvalue(6,5,4,3,2,4));
