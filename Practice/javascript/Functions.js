2245a7e51ecc965152efb947fd23c48d1fda9f07
vikasreddycode <vikasmudimela@gmail.com>
2023-04-18 11:45:49 +0530

    iterating through the object loop

--- Practice/javascript/Functions.js
+++ Practice/javascript/Functions.js
@@ -13,3 +13,23 @@ let name = (v1,v2) => {
     console.log("this is a lambda function with parameters",v1,v2);
 }
 name(2,3);
+let vicky = 
+{
+    name:'vikas',
+    role:'user',
+    time : '3:30',
+    methodk : 'hello'
+};
+// console.log(vicky['name']);
+// console.log(vicky.name);
+// vicky.role='admin30';
+// console.log(vicky.role);
+for (let x in vicky)
+{
+    console.log(x);
+}
+for (let x in vicky)
+{
+    console.log(vicky[x]);
+    console.log(x);
+}
