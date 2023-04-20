1936ac3b6d0d6e22d6f58a3f275e30209326ef9c
vikasreddycode <vikasmudimela@gmail.com>
2023-04-20 16:55:12 +0530

    nw assignment'

--- Practice/javascript/Objectprototype.js
+++ Practice/javascript/Objectprototype.js
@@ -1,9 +1,16 @@
-const bankaccountmoney ={
-    money:20000,
-    acc:1234977
-};
-const depo = {
-    amtdepo : 23000,
-    dcus: 'vicky',
-    __proto__:bankaccountmoney
-};
+// const bankaccountmoney ={
+//     money:20000,
+//     acc:1234977
+// };
+// const depo = {
+//     amtdepo : 23000,
+//     dcus: 'vicky',
+    
+// };
+// depo.__proto__=bankaccountmoney;
+String.prototype.truelength =  function()
+{
+    console.log(`the length of string is ${this.trim().length}`);
+}
+let m = "vicky";
+m.truelength();
