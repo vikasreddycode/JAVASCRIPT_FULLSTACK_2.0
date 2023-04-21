63a4454966c5a97c16f5171bd669a873d6db584e
vikasreddycode <vikasmudimela@gmail.com>
2023-04-21 16:57:37 +0530

    truth and false statements

---
+++ Javascript_Assignments/08_ques.js
@@ -0,0 +1,3 @@
+let vikcy = " his  name is vikas ";
+console.log(`the string without using trim method is ${vikcy.length}`);
+console.log(`he string without using trim method is ${vikcy.trim().length}`) ;
