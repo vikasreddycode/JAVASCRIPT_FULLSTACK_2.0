fdbf322cd86d159497d0ac01d692a86bed93217d
vikasreddycode <vikasmudimela@gmail.com>
2023-04-21 14:58:39 +0530

    includes method

---
+++ Javascript_Assignments/04_Question/variables.js
@@ -0,0 +1,8 @@
+let language = "we are studying javaScript";
+if(language.includes('Script'))
+{
+    console.log(`the string ${language} conatins the word Script`);
+}
+else{
+    console.log(`the string ${language} doesnot conatin the word Script`);
+}
