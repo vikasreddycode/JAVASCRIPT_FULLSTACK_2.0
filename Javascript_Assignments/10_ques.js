fbd1a4c6d024cb49938fb3653d9e7f9d5c563e38
vikasreddycode <vikasmudimela@gmail.com>
2023-04-21 17:06:04 +0530

    truth and false statements2

--- Javascript_Assignments/10_ques.js
+++ Javascript_Assignments/10_ques.js
@@ -9,6 +9,10 @@ console.log(`${4!==4}`);
 console.log(`${4!='4'}`);
 console.log(`${4=='4'}`);
 console.log(`${4==='4'}`);
+console.log(`${"python".length}`);
+console.log(`${"jargon".length}`);
+console.log(`${'python'==='jargon'}`);
+
 
 
 
