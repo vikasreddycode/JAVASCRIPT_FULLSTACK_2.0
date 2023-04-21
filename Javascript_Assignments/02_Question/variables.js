6b74e59404934f22d0ba93603c430561199bd0c4
vikasreddycode <vikasmudimela@gmail.com>
2023-04-21 14:46:50 +0530

    2ques

---
+++ Javascript_Assignments/02_Question/variables.js
@@ -0,0 +1,6 @@
+let first_name = "vikas";
+let lastn_name= "reddy";
+let marital_status="unmarried";
+let country = "India";
+let age = 20;
+console.log(`the firstname is ${first_name} and last_name is ${lastn_name} and marital_staus is ${marital_status} and the country is ${country} and age is ${age}`);
