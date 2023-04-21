d0f488e0c2b4d3c49c489b69c1f7809cff75ce26
vikasreddycode <vikasmudimela@gmail.com>
2023-04-21 14:22:18 +0530

    newfile

---
+++ Javascript_Assignments/variables.js
@@ -0,0 +1,8 @@
+let name = "vicky";
+let truthvalue = false;
+let m = undefined;
+let l = null;
+console.log(`the string value is ${name} and the datatype is ${typeof name}`);
+console.log(`the value of truthvalue is ${truthvalue} and the datatype is ${ typeof truthvalue}`);
+console.log(`the value of m is ${m} and its datatype is ${typeof m}`);
+console.log(`the value of l is ${l} and its datatype is ${typeof l}`);
