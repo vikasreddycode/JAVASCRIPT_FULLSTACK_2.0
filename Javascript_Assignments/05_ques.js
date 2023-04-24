add91c10ac05c725d2526f78f5f927da5184e7df
vikasreddycode <vikasmudimela@gmail.com>
2023-04-24 15:42:15 +0530

    new modification

--- Javascript_Assignments/05_ques.js
+++
@@ -1,4 +0,0 @@
-let name  = "This is a string";
-let splitarray = Array.from(name.split(" "));
-let newarray = name.split(" ");
-console.log(`${typeof splitarray === typeof newarray}`)
--- Javascript_Assignments/08_ques.js
+++
@@ -1,3 +0,0 @@
-let vikcy = " his  name is vikas ";
-console.log(`the string without using trim method is ${vikcy.length}`);
-console.log(`he string without using trim method is ${vikcy.trim().length}`) ;
