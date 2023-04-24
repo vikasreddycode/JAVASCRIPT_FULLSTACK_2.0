3639d90b8e0f4ec7460217ccf8f54ef4cc70ca09
vikasreddycode <vikasmudimela@gmail.com>
2023-04-24 15:44:18 +0530

    6

---
+++ Javascript_Assignments/06_ques/06_question.js
@@ -0,0 +1,6 @@
+let str1 = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let k = Array.from(str1.split(","));
+let str2 = str1.split(",");
+console.log(k);
+console.log(str2);
+// console.log(`${Array.from(str1.split(","))}`);
