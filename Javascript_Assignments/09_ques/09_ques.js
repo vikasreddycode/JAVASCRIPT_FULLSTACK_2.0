47748d414901c2def101dd03950f041f41e007bf
vikasreddycode <vikasmudimela@gmail.com>
2023-04-24 15:45:53 +0530

    9

---
+++ Javascript_Assignments/09_ques/09_ques.js
@@ -0,0 +1,6 @@
+console.log(`the truth statement is ${'vicky'==="vicky"}`);
+console.log(`the truth statement is ${'vicky   '.length>="vicky".length}`);
+console.log(`the truth statement is ${'vicky '.length!="vicky".length}`);
+console.log(`the truth statement is ${'vicky  '.trim().length!="vicky".length}`);
+console.log(`the truth statement is ${'2'===2}`);
+console.log(`the truth statement is ${3<2}`);
