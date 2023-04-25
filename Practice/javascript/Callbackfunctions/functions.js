a23ff8373d3dfdc8643c58d213e1de47a51dc566
vikasreddycode <vikasmudimela@gmail.com>
2023-04-25 13:26:17 +0530

    new set Intervale for a certain activity to occur for repetitive inetrval

--- Practice/javascript/Callbackfunctions/functions.js
+++ Practice/javascript/Callbackfunctions/functions.js
@@ -1,23 +1,23 @@
-// // const h = (n) =>
+// // // const h = (n) =>
+// // // {
+// // //     return n**2;
+// // // }
+// // // function  cube(h,n)
+// // // {
+// // // return h(n)*n;
+// // // }
+// // // let k = cube(h,3);
+// // // console.log(k);
+// // let array  = [1,2,3,4,5,6,7,7,8];
+// // function func(a)
 // // {
-// //     return n**2;
+// //     console.log(a);
 // // }
-// // function  cube(h,n)
-// // {
-// // return h(n)*n;
-// // }
-// // let k = cube(h,3);
-// // console.log(k);
-// let array  = [1,2,3,4,5,6,7,7,8];
-// function func(a)
-// {
-//     console.log(a);
-// }
-// array.forEach(func);
-// //set timepout 
-setTimeout(()=>{
-console.log("helo wama");
-},3000) ;
-setInterval(()=>{
-console.log("hello");
-},3000)
+// // array.forEach(func);
+// // //set timepout 
+// setTimeout(()=>{
+// console.log("helo wama");
+// },3000) ;
+// setInterval(()=>{
+// console.log("hello");
+// },3000)
