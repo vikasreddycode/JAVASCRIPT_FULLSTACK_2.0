72b8460e5ab68a62ede72291aa9d24632c2b7696
vikasreddycode <vikasmudimela@gmail.com>
2023-04-26 16:06:17 +0530

    spreading of data into chunks using ...variable name

--- Practice/javascript/Callbackfunctions/functions.js
+++ Practice/javascript/Callbackfunctions/functions.js
@@ -1,37 +1,8 @@
-// // // // // const h = (n) =>
-// // // // // {
-// // // // //     return n**2;
-// // // // // }
-// // // // // function  cube(h,n)
-// // // // // {
-// // // // // return h(n)*n;
-// // // // // }
-// // // // // let k = cube(h,3);
-// // // // // console.log(k);
-// // // // let array  = [1,2,3,4,5,6,7,7,8];
-// // // // function func(a)
-// // // // {
-// // // //     console.log(a);
-// // // // }
-// // // // array.forEach(func);
-// // // // //set timepout 
-// // // setTimeout(()=>{
-// // // console.log("helo wama");
-// // // },3000) ;
-// // // setInterval(()=>{
-// // // console.log("hello");
-// // // },3000)
-// // let arr = [1,2,3];
-// // let k =arr.map(func);
-// // function func(num)
-// // {
-// //     return Number(num)+Number(3);
-// // }
-// // console.log(k);
-// //filter function
-// const count = ["India","Vickyland","DisneyLand","Marvel"];
-// const vel = count.filter((i) => i.includes("Land"));
-// console.log(vel);
-const val = [1,2,3,4,5,6,7,8];
-let m = val.reduce((acc,curr)=> acc*curr,1);
-console.log(m);
+// 
+function sumone(x,y)
+{
+    return x+y;
+}
+let j = [2,3,4];
+let b = sumone(...j);
+console.log(b);
