d127c0803f3901471e6bb2badc5e95dabfb7a403
vikasreddycode <vikasmudimela@gmail.com>
2023-04-26 17:43:01 +0530

    throwing errors artificially

--- Practice/javascript/Callbackfunctions/functions.js
+++ Practice/javascript/Callbackfunctions/functions.js
@@ -1,21 +1,36 @@
-// function sum2(...args)
+// // function sum2(...args)
+// // {
+// //     console.log(args);
+// //     // let sum = 0;
+// //     let sum =args.reduce((acc,curr) => acc + curr,0);
+// //     return sum;
+// // }
+// // let b = sum2(1,2,3,4,5,6);
+// // console.log(b);
+// try
 // {
-//     console.log(args);
-//     // let sum = 0;
-//     let sum =args.reduce((acc,curr) => acc + curr,0);
-//     return sum;
+// let m = 5/0;
+// console.log(m1);
 // }
-// let b = sum2(1,2,3,4,5,6);
-// console.log(b);
+// catch (error1)
+// {
+//     console.log(error1.name);
+// }
+// finally{
+//     console.log("i will always run");
+// }
+function func(width)
+{
+    
+    if(isNaN(width))
+    {
+        throw new Error("enter a number");
+    }
+}
 try
 {
-let m = 5/0;
-console.log(m1);
-}
-catch (error1)
+    func("vicky");
+}catch(e)
 {
-    console.log(error1.name);
-}
-finally{
-    console.log("i will always run");
+    console.log(e);
 }
