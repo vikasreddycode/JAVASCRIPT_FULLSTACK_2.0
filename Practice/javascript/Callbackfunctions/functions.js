08cb685d7795b82e36cb7cbde0d428272e82510b
vikasreddycode <vikasmudimela@gmail.com>
2023-04-26 17:11:28 +0530

    error handling'

--- Practice/javascript/Callbackfunctions/functions.js
+++ Practice/javascript/Callbackfunctions/functions.js
@@ -1,8 +1,18 @@
-// 
-function sumone(x,y)
+// function sum2(...args)
+// {
+//     console.log(args);
+//     // let sum = 0;
+//     let sum =args.reduce((acc,curr) => acc + curr,0);
+//     return sum;
+// }
+// let b = sum2(1,2,3,4,5,6);
+// console.log(b);
+try
 {
-    return x+y;
+let m = 5/0;
+console.log(m1);
 }
-let j = [2,3,4];
-let b = sumone(...j);
-console.log(b);
+catch (error)
+{
+    console.log(error1.name);
+}
