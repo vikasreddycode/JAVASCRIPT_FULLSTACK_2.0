86947007b5c5010e73195add0f8f9cc1007d5e53
vikasreddycode <vikasmudimela@gmail.com>
2023-04-27 11:00:53 +0530

    settimeout function

--- Practice/javascript/Callbackfunctions/functions.js
+++ Practice/javascript/Callbackfunctions/functions.js
@@ -1,36 +1,55 @@
-// // function sum2(...args)
+// // // // function sum2(...args)
+// // // // {
+// // // //     console.log(args);
+// // // //     // let sum = 0;
+// // // //     let sum =args.reduce((acc,curr) => acc + curr,0);
+// // // //     return sum;
+// // // // }
+// // // // let b = sum2(1,2,3,4,5,6);
+// // // // console.log(b);
+// // // try
+// // // {
+// // // let m = 5/0;
+// // // console.log(m1);
+// // // }
+// // // catch (error1)
+// // // {
+// // //     console.log(error1.name);
+// // // }
+// // // finally{
+// // //     console.log("i will always run");
+// // // }
+// // function func(width)
 // // {
-// //     console.log(args);
-// //     // let sum = 0;
-// //     let sum =args.reduce((acc,curr) => acc + curr,0);
-// //     return sum;
-// // }
-// // let b = sum2(1,2,3,4,5,6);
-// // console.log(b);
-// try
-// {
-// let m = 5/0;
-// console.log(m1);
-// }
-// catch (error1)
-// {
-//     console.log(error1.name);
-// }
-// finally{
-//     console.log("i will always run");
-// }
-function func(width)
-{
     
-    if(isNaN(width))
-    {
-        throw new Error("enter a number");
-    }
-}
-try
-{
-    func("vicky");
-}catch(e)
-{
-    console.log(e);
-}
+// //     if(isNaN(width))
+// //     {
+// //         throw new Error("enter a number");
+// //     }
+// // }
+// // try
+// // {
+// //     func("vicky");
+// // }catch(e)
+// // {
+// //     console.log(e);
+// // }
+// //event loop
+// const userOne = ( ) =>
+// {
+//     console.log("this is userone");
+// };
+// const userTwo = () =>
+// {
+//     setTimeout(() => {
+//   console.log("hello i am inside");
+//     },3000)
+//     console.log("this is usertwo");
+// }
+// const userThree = () =>
+// {
+//     console.log("this is userthree");
+// }
+// userOne();
+// userTwo();
+// userThree();
